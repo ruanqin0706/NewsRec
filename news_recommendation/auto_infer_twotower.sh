27b3e20580a767311b2053d2bf3e59b7dec21b8c
Q~ <rqrqrrq@qq.com>
2024-02-23 09:27:41 +0000

    modify auto_infer_twotower.sh

--- news_recommendation/auto_infer_twotower.sh
+++ news_recommendation/auto_infer_twotower.sh
@@ -1,27 +1,6 @@
 #!/bin/bash
+
 #PyTorch / 1.9.1 / 11.1.1 / 3.8
-
-mkdir -p /root/MIND
-
-unzip /root/autodl-nas/MINDlarge_train.zip -d /root/MIND/MINDlarge_train/
-
-unzip /root/autodl-nas/MINDlarge_dev.zip -d /root/MIND/MINDlarge_dev/
-
-unzip /root/autodl-nas/MINDlarge_utils.zip -d /root/MIND/MINDlarge_utils/
-
-unzip /root/autodl-nas/MINDlarge_test.zip -d /root/MIND/MINDlarge_test/
-
-unzip /root/autodl-nas/large_TwoTower-AllBert-Rnn.zip -d /root/MIND/large_TwoTower-AllBert-Rnn/
-
-unzip /root/autodl-nas/News-Recommendation.zip -d /root
-
-unzip /root/autodl-nas/mind_candidate_news_201911_interest_without_labels.zip -d /root/autodl-tmp/
-unzip /root/autodl-nas/mind_candidate_news_201911_biasprop_without_labels.zip -d /root/autodl-tmp/
-
-cd /root/News-Recommendation/src/
-pip install transformers
-pip install pandas
-pip install sklearn
 
 cd /root/News-Recommendation/src/
 
